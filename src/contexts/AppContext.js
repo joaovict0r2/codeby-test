8a03929e839636b5c4345d24e7a059d23cd5793f
João Victor <joaov.lionsoft@gmail.com>
2022-06-25 01:53:40 -0300

    Add current theme at localStorage

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -1,11 +1,17 @@
 import {
-  useState, createContext, useMemo, 
+  useState, createContext, useMemo, useEffect, 
 } from 'react';
 
 export const AppContext = createContext({});
 
 export function AppProvider({ children }) {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') || 'light',
+  );
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
   function handleToggleTheme() {
     setTheme((prevState) => (prevState === 'dark' ? 'light' : 'dark'));
